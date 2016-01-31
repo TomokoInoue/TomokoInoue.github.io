35c7bb90b053b47e541f8f07d705e1aa7fb7d273
井上智子 <tomo.inoue0521@gmail.com>
2016-01-31 15:20:43 +0900

    touch

---
+++ js/touch.js
@@ -0,0 +1,17 @@
+//DOMロード
+$(function(){
+
+	// タッチイベント
+	$('#ball').on('touchstart',function(){
+		$('#touchResult li').eq(0).text('touchstartしたよ');
+	});
+
+	$('#ball').on('touchmove',function(){
+		$('#touchResult li').eq(1).text('touchmoveしたよ');
+	});
+
+	$('#ball').on('touchend',function(){
+		$('#touchResult li').eq(2).text('touchendしたよ');
+	});
+
+});
