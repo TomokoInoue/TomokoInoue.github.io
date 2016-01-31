ffec07c2eeb6a96a41f973d21fa4b9ca659db1aa
井上智子 <tomo.inoue0521@gmail.com>
2016-01-31 15:37:43 +0900

    touch

--- js/touch.js
+++ js/touch.js
@@ -1,6 +1,23 @@
 //DOMロード
 $(function(){
 
+	// 緯度・経度
+	var lon;
+	var lat;
+
+
+	// GPS
+	navigator.geolocation.getCurrentPosition(function(evt){
+		// 緯度
+		var lon = evt.coords.longitude;
+		// 経度
+		var lat = evt.coords.longitude;
+	},
+	function(erro){
+		console.log('GPS取れなかったよー');
+	});
+
+
 	// タッチイベント
 	$('#ball').on('touchstart',function(){
 		$('#touchResult li').eq(0).text('touchstartしたよ');
@@ -12,6 +29,16 @@ $(function(){
 
 	$('#ball').on('touchend',function(){
 		$('#touchResult li').eq(2).text('touchendしたよ');
+
+		$('#geo li').eq(0).text('緯度: ' + lon);
+		$('#geo li').eq(1).text('経度: ' + lat);
 	});
+
+
+
+
+
+
+
 
 });
