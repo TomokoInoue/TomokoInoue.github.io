ba0a7bea601f73f569b1041698cf010b24a4ae2a
井上智子 <tomo.inoue0521@gmail.com>
2016-02-07 11:41:56 +0900

    add ajax

--- 20160207/javascript/weatherApi/js/script.js
+++ 20160207/javascript/weatherApi/js/script.js
@@ -5,5 +5,20 @@ $(function(){
 		var lon = pos.coords.longitude;
 
 		console.log(lat,lon);
+
+		// 定数
+		const apikey = '9600dda65548f3a5f458aa5ab8721019';
+		// openweathermapのお天気情報のURL
+		var url = 'api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apikey;
+
+
+		$.ajax({
+			url:url,
+			dataType:'json'
+		}).done(function(data){
+
+		}).fail(function(){
+			console.log('ajax error!!!');
+		});
 	});
 });
