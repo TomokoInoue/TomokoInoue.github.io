577a95725831ad490ae73514d0ef520f533e8de3
井上智子 <tomo.inoue0521@gmail.com>
2016-02-07 11:54:33 +0900

    add ajax

--- 20160207/javascript/weatherApi/js/script.js
+++ 20160207/javascript/weatherApi/js/script.js
@@ -36,6 +36,10 @@ $(function(){
 			var areaTemp = data.main.temp;
 			areaTemp = areaTemp - 273.15;
 			$('#areaTemp').text(Math.round(areaTemp) + '度');
+
+			// 湿度
+			var areaHumidity = data.main.humidity;
+			$('#areaHumidity').text(areaHumidity + '%');
 		}
 	});
 });
