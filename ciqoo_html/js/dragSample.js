41547695a8f6ba6da9bcc39f1f1c6a4b65e383f8
井上智子 <tomo.inoue0521@gmail.com>
2016-02-10 21:13:20 +0900

    add

--- ciqoo_html/js/dragSample.js
+++ ciqoo_html/js/dragSample.js
@@ -5,6 +5,7 @@ var stage;
 function init() {
 	// ステージの作成
 	stage = new createjs.Stage('canvasEl');
+	console.log('ステージ読み込んだよ');
 
 	var circle = new createjs.Shape();
 	circle.graphics.beginFill('DeepSkyBlue').drawCircle(100, 100, 50);
@@ -18,6 +19,8 @@ function init() {
 		var instance = evt.target;
 		instance.x = evt.stageX;
 		instance.y = evt.stageY;
+
+		stage.update();
 	}
 
 
