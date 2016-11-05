c1fb4e24e6e0321b0b0b8e4d0a579f7b12ae701a
TomokoInoue <inoue0521.backlog.jp>
2016-11-05 16:32:46 +0900

    [add] js独立ファイルの追加

---
+++ toppage_btn/js/script.js
@@ -0,0 +1,34 @@
+// 即時関数
+(function() {
+
+	// DOMの読み込み
+	$(function() {
+
+		// #back_to_topを消す
+		$('#back_to_top').hide();
+
+		// 一定の位置までスクロールされた時にボタンを表示、非表示
+		$(window).scroll(function() {
+
+			// 左下に取得したスクロールの垂直位置の数字をテキストで表示
+			$('#pos').text($(this).scrollTop());
+
+			// 取得したスクロールの垂直位置が60より大きかった場合
+			if ($(this).scrollTop() > 60) {
+				$('#back_to_top').fadeIn(); // フェードして表示
+			} else {
+				$('#back_to_top').fadeOut(); // フェードして非表示
+			}
+		});
+
+
+		// ページトップボタンがクリックされたら上に戻る処理
+		$('#back_to_top a').click(function() {
+			// スクロールの垂直位置が0にくるようアニメーション
+			$('body').animate({
+				scrollTop:0
+			}, 500);
+			return false;
+		})
+	});
+});
